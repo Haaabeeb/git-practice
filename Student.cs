24678eb88529bd335e88adbc3d47dea75b34f65a
haaabeeb <habeebaliahmad284200501@gmail.com>
2026-08-17 14:14:56 +0300

    added student class

---
+++ Student.cs
@@ -0,0 +1,20 @@
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Grade { get; set; }
+
+    public Student(string name, int age, double grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Grade: {Grade}");
+    }
+}
